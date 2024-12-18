96c96eeeca0e42007f6343b3bdd0ae89997c18a4
github-cloudlabsuser-1363 <168110128+github-cloudlabsuser-1363@users.noreply.github.com>
2024-12-18 04:58:54 +0000

    Added Python Calculator Script

--- Calculator.py
+++ Calculator.py
@@ -9,7 +9,7 @@ def multiply(x, y):
 
 def divide(x, y):
     if y == 0:
-        raise ValueError("Cannot divide by zero!")
+        return "Error! Division by zero."
     return x / y
 
 def main():
@@ -32,10 +32,7 @@ def main():
         elif choice == '3':
             print(f"{num1} * {num2} = {multiply(num1, num2)}")
         elif choice == '4':
-            try:
-                print(f"{num1} / {num2} = {divide(num1, num2)}")
-            except ValueError as e:
-                print(e)
+            print(f"{num1} / {num2} = {divide(num1, num2)}")
     else:
         print("Invalid input")
 
